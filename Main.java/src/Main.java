34e4cf57667ddb06e776868779d981408c5f3991
SaadKhan <mskscotland@gmail.com>
2026-01-29 12:55:59 +0000

    This is the main.java file

---
+++ Main.java/src/Main.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        tester newtester = new tester();
+        System.out.println(newtester.getResult());
+    }
+}
---
+++ Main.java/src/tester.java
@@ -0,0 +1,13 @@
+public class tester {
+    char result;
+
+    public tester() {
+
+        result = 'a' + 3;
+    }
+
+    public char getResult(){
+
+        return result;
+    }
+}
