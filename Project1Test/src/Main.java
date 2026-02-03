eab75293eeae34f3c96ba35adcfc70463c562e57
Finlay Robb CESM2024 <finlay.robb.2024@uni.strath.ac.uk>
2026-02-03 16:04:39 +0000

    Removed test folder

--- Project1Test/src/Main.java
+++
@@ -1,8 +0,0 @@
-import org.w3c.dom.ls.LSOutput;
-
-public class Main {
-    public static void main(String[] args) {
-        tester newtester = new tester();
-        System.out.println(newtester.getResult());
-    }
-}
